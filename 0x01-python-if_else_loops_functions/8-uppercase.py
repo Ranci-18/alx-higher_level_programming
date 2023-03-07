49675e3fe724c37a27acdd12c3634dcf1662f16b
Frank <wanyoike39@gmail.com>
2023-03-08 02:56:30 +0300

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            offset_value = 32
+        else:
+            offset_value = 0
+        print("{:c}".format(ord(str[i]) - offset_value), end='')
+    print()
+            
