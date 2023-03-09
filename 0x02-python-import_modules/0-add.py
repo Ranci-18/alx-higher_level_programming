601952cb0924a9197b0cd4227b9a2d79dc9cad3c
Frank <wanyoike39@gmail.com>
2023-03-09 12:50:22 +0300

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 import add_0 as add
-sum = add.add(1, 2)
-print("{:d} + {:d} = {:d}".format(1, 2, sum))
+a = 1
+b = 2
+sum = add.add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, sum))
