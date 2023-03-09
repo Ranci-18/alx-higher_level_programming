8f0835d0e86630c1ff576ad62b46654c44801da4
Frank <wanyoike39@gmail.com>
2023-03-09 16:25:47 +0300

    taskk 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv) - 1
+    if args == 0:
+        print("{} arguments.".format(args))
+    elif args == 1:
+        print("{} argument:".format(args))
+        print("{}: {}".format(args, sys.argv[args]))
+    else:
+        print("{} arguments.".format(args))
+        for i in range(1, args + 1):
+            print("{}: {}".format(i, sys.argv[i]))
