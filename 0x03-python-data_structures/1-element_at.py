5b3b7d067e56dec3f5326e4ca30e61caf8718d83
Frank <wanyoike39@gmail.com>
2023-03-12 11:52:12 +0300

    task 1 - get element at idx

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for i in range(len(my_list)):
+        if idx < 0:
+            return None
         if i == idx and idx > 0:
             return my_list[i]
-        return None
