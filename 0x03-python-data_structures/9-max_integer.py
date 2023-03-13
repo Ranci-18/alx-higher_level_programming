3d3d3a51b423677d03aac655c701ca8393f796bd
Frank <wanyoike39@gmail.com>
2023-03-13 11:41:12 +0300

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    biggest_int = 0
+    biggest_int = my_list[0]
     list_len = len(my_list)
 
     if list_len == 0:
@@ -9,4 +9,5 @@ def max_integer(my_list=[]):
         for i in range(list_len):
             if my_list[i] > biggest_int:
                 biggest_int = my_list[i]
+
         return biggest_int
