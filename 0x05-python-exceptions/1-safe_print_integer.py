0575684ab2a1100608d842137b3577372402784b
Frank <wanyoike39@gmail.com>
2023-03-27 17:54:37 +0300

    task 1 else

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,6 @@ def safe_print_integer(value):
     if isinstance(value, int):
         try:
             print("{:d}".format(value))
+            return True
         except TypeError:
             return False
-        else:
-            return True
