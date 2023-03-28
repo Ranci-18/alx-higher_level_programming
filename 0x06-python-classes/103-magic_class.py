6cfd8a42b6b7396322b256fd62ad4a5dfacc9a86
Frank <wanyoike39@gmail.com>
2023-03-29 00:32:33 +0300

    advanced task bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -11,10 +11,23 @@ class MagicClass:
 
         Attributes: radius
         """
-        self.__radius = 0
         if not isinstance(radius, int) and not isinstance(radius, float):
             raise TypeError('Radius must be a number')
         self.__radius = radius
+
+    @property
+    def radius(self):
+        """returns the radius
+        """
+        return self.__radius
+
+    @radius.setter
+    def radius(self, value):
+        """sets the radius of MagicClass
+        """
+        if type(value) is not int and type(value) is not float:
+            raise TypeError('Radius must be a number')
+        self.__radius = value
 
     def area(self):
         """Method computes the area of MagicClass
@@ -25,5 +38,5 @@ class MagicClass:
     def circumference(self):
         """Method computes circumference of MagicClass
         """
-        result_circumference = 2 * math.pi * (self.__radius)
+        result_circumference = 2 * math.pi * self.__radius
         return result_circumference
