0bbe612f24fae113d65a23a05538b4d0d3c347c4
Frank <wanyoike39@gmail.com>
2023-03-28 18:00:53 +0300

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""defines a class square"""
+
+
+class Square:
+    """
+    class Square defining a square of 'size'
+
+    Attributes: size
+    """
+    def __init__(self, size=0):
+        """
+        creates first instance of Square
+
+        Args: size
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
+
+    def area(self):
+        """public method computing the area of square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Method to return the size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Method to set the object of square object
+
+        Raises:
+            TypeError
+            ValueError
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
