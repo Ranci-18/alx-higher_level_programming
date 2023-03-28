994e09d95e9a4917b8dfdd4cf3b8ebe3bfedce51
Frank <wanyoike39@gmail.com>
2023-03-29 00:17:55 +0300

    advanced task bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""definition of MagicClass"""
+
+
+import math
+class MagicClass:
+    """MagicClass performs some operations
+    """
+    def __init__(self, radius=0):
+        """New instance of MagicClass
+
+        Attributes: radius
+        """
+        if not isinstance(radius, int) or not isinstance(radius, float):
+            raise TypeError("Radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Method computes the area of MagicClass
+        """
+        result_area = math.pi * self.__radius ** 2
+        return result_area
+
+    def circumference(self):
+        """Method computes circumference of MagicClass
+        """
+        result_circumference = math.pi * self.__radius * 2
+        return result_circumference
