69b751c68a4fcba83807fa032f8728b7305b5e03
Frank <wanyoike39@gmail.com>
2023-03-29 14:39:41 +0300

    fixed advanced code - safe print integer err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ def safe_print_integer_err(value):
     if isinstance(value, int):
         try:
             print("{:d}".format(value))
-        except (ValueError, TypeError) as err:
+        except Exception as err:
             sys.stderr.write("Exception: {}\n".format(err))
             return False
         else:
