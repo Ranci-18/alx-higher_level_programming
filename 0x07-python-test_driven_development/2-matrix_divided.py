8c2d5a90248617f62a078b6f666045f5aff49cfd
Frank <wanyoike39@gmail.com>
2023-04-05 04:53:02 +0300

    task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,22 +13,19 @@ def matrix_divided(matrix, div):
     Returns:
         new matrix of divided elements
     """
-    rws = len(matrix)
-    clmns = len(matrix[0])
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    if not all(isinstance(element, (float, int)) for row in matrix for element in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    for elements in matrix:
-        if not isinstance(elements, int) and not isinstance(elements, float):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    for i in rws:
-        if i < rws - 1 and matrix[i] != matrix[i + 1]:
+    if not all(len(row) == len(matrix[0]) for row in matrix):
             raise TypeError("Each row of the matrix must have the same size")
+
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     
-    for i in rws:
-        for j in clmns:
-            result = [[round(matrix[i][j] / div, 2) for j in clmns] for i in rws]
-
+    result = [[round(element / div, 2) for element in row] for row in matrix]
     return result
