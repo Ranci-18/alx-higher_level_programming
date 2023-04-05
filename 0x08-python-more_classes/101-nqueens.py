e157859139cc801958daf2043945d269137c8ed9
Frank <wanyoike39@gmail.com>
2023-04-06 01:01:03 +0300

    advanced task

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -91,7 +91,9 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         sys.exit(1)
 
-    if not isinstance(int(sys.argv[1]), int):
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
 
