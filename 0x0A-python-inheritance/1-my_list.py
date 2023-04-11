08447797075087c15999dc7da9475f021fd924d8
Frank <wanyoike39@gmail.com>
2023-04-11 18:44:37 +0300

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defining a subclass"""
+
+
+class MyList(list):
+    """Subclass MyList of superclass list
+    """
+    def print_sorted(self):
+        """Method prints sorted list
+        """
+        print(sorted(self))
