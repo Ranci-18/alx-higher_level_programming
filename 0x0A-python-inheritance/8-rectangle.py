9cf8b5478165042375fd5d581a9a74aa4707c789
Frank <wanyoike39@gmail.com>
2023-04-11 22:16:02 +0300

    task 8 first commit

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""defining an empty class and subclass"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        """Method raise Exception error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method validates value
+
+        Raise:
+            TypeError
+            ValueError
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """Subclass Rectangle
+
+        Attributes:
+            width and height
+    """
+    def __init__(self, width, height):
+        """Instantiation"""
+        super().__init__()
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
