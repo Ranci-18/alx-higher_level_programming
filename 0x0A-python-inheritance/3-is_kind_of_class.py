190a1ad3a58aebf870ccd2271b43868da944d403
Frank <wanyoike39@gmail.com>
2023-04-11 19:28:19 +0300

    task 2

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defining a function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function checks if an object is 
+    of a superclass or subclass
+    
+    Returns:
+        True or False
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
