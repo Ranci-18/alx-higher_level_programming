7bd86efe312dc0623141c41fed1a6fee7c0b3846
Frank <wanyoike39@gmail.com>
2023-04-11 20:45:34 +0300

    task 5

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defining an empty class"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        """Method raise Exception error"""
+        raise Exception("area() is not implemented")
