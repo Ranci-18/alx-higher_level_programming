5d9a039869fe6c4b93c84cf7d2297587632253e1
Frank <wanyoike39@gmail.com>
2023-04-12 18:20:40 +0300

    first task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """function reads a text file and prints to stdout"""
-    with open(filename, 'r') as UTF8:
-        contents = UTF8.read()
-        print(contents)
+    with open(filename, 'r') as f:
+        contents = f.read()
+        print(contents, end='')
