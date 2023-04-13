45c927fb081a28eb6a00afaa5d74572f22b96bf0
Frank <wanyoike39@gmail.com>
2023-04-13 06:04:52 +0300

    advanced task 1

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defining a function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """function searches and appends new string
+    after search string"""
+    with open(filename, 'r') as f:
+        content = f.readlines()
+    
+    with open(filename, 'w') as o_f:
+        for line in content:
+            if line == search_string:
+                o_f.write(line)
+                o_f.write(new_string)
+            else:
+                o_f.write(line)
