1092f2c23a43bc74e086749c757d423660700219
Frank <wanyoike39@gmail.com>
2023-04-13 03:26:57 +0300

    eleventh task

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""defining a class student"""
+
+
+class Student:
+    """class Student
+    Attributes:
+        first_name, last_name, age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Methhod retrieves dict representation of Student
+        instance"""
+        if attrs is not None:
+            my_dict = {}
+            for key in attrs:
+                try:
+                    my_dict[key] = self.__dict__[key]
+                except KeyError:
+                    pass
+            return my_dict        
+        return self.__dict__
