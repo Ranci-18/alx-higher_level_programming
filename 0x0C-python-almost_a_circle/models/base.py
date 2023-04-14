bc45508372d12f8103149e60e731584358442f4b
Frank <wanyoike39@gmail.com>
2023-04-14 14:28:12 +0300

    initial project commits

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""defining a class"""
+
+
+class Base:
+    """Class Base
+    Attributes:
+        private class attribute
+        id
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """instantiation of class object"""
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
