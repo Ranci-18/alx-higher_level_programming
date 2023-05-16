703ce569a883ddcd7b9caf222ff56f1f76f0da17
Frank <wanyoike39@gmail.com>
2023-05-16 21:40:55 +0300

    Advanced tasks

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- displaying the top 3 city temps
+-- during July and August ordered by temp
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
