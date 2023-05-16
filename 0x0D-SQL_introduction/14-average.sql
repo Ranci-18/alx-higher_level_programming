8d1557e23628ff9903a67f3215ea946fe015ea0e
Frank <wanyoike39@gmail.com>
2023-05-16 20:53:36 +0300

    task 14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,7 +1,4 @@
 -- computing of scores of all records
 -- and adding them to average column
-ALTER TABLE second_table
-ADD COLUMN average float;
-INSERT INT second_table (average)
-SELECT AVG(score)
+SELECT AVG(score) AS average
 FROM second_table;
