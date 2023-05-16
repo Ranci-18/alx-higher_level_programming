d1b480465661bcf063f3d87e533ab9406f35392d
Frank <wanyoike39@gmail.com>
2023-05-16 20:30:06 +0300

    task 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- listing records with a score >= 10 in second_table
 -- by score and name
-SELECT score, name FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
