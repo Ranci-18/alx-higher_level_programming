2a60877830bb29d67b63eba73c7b41a69011212b
Frank <wanyoike39@gmail.com>
2023-05-16 15:40:47 +0300

    first five tasks

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 --creates tables first_table in current
 --database if it doesn't exist
-CREATE TABLE IF NOT EXISTS first_table ( 
+CREATE TABLE IF NOT EXISTS first_table( 
     id INT, 
     name VARCHAR(256)
     );
