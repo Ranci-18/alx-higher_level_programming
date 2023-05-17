8b20674d352d48750d575f77578a12ef30d0748f
Frank <wanyoike39@gmail.com>
2023-05-17 19:33:39 +0300

    upto task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,4 +3,4 @@ SELECT
     tv_shows.title AS title FROM tv_show.genres
     INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
     INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id AND tv_genres.name = 'Comedy'
-    ORDER BY title;
+    ORDER BY title ASC;
