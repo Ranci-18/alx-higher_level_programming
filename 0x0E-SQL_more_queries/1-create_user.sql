6fa6e265e6422912c3bcbb80745a6a6d5e8dd087
Frank <wanyoike39@gmail.com>
2023-05-17 12:09:35 +0300

    upto task 7

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- query creates a SQL server user user_0d_1 and grants all
 -- privileges to SQL server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON '.' TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
