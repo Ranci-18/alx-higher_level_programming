01a2e5048a6acd2a1c81c32ba22f9e1658acc28f
Frank <wanyoike39@gmail.com>
2023-06-13 17:45:18 +0300

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,18 +4,19 @@ const square = require('./5-square');
 class Square extends square {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    let i;
-    let j;
-    let row;
+      this.print();
+    } else {
+      let i;
+      let j;
+      let row;
 
-    for (i = 0; i < this.height; i++) {
-      row = '';
-      for (j = 0; j < this.width; j++) {
-        row += c;
+      for (i = 0; i < this.height; i++) {
+        row = '';
+        for (j = 0; j < this.width; j++) {
+          row += c;
+        }
+        console.log(row);
       }
-      console.log(row);
     }
   }
 }
