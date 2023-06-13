3aab74efc215031ac104b283358d5523983d3b91
Frank <wanyoike39@gmail.com>
2023-06-13 22:37:40 +0300

    advanced task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,6 +5,6 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-const data1 = fs.readFileSync(fileA, {encoding: 'utf-8'});
-const data2 = fs.readFileSync(fileB, {encoding: 'utf-8'});
-fs.writeFileSync(fileC, data1 + data2, {encoding: 'utf-8'});
+const data1 = fs.readFileSync(fileA, { encoding: 'utf-8' });
+const data2 = fs.readFileSync(fileB, { encoding: 'utf-8' });
+fs.writeFileSync(fileC, data1 + data2, { encoding: 'utf-8' });
