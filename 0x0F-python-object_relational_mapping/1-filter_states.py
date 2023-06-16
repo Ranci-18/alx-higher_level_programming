fb08156756d6436a0bc66c97e5f6141b7d39c1f7
Frank <wanyoike39@gmail.com>
2023-06-17 00:02:22 +0300

    second task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
                 "name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close
     connct.close
