eddff966ab92b8c7379460160c221890458d6e77
Frank <wanyoike39@gmail.com>
2023-06-17 01:22:46 +0300

    sixth task

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
                              passwd="root", db=sys.argv[3], charset="utf8")
     cur = connct.cursor()
     cur.execute("SELECT cities.name FROM cities \
-    JOIN state ON cities.state_id = states.id \
+    JOIN states ON cities.state_id = states.id \
     WHERE states.name LIKE %s ORDER BY cities.id", (sys.argv[4],))
     rows = cur.fetchall()
     print(", ".join(city[0] for city in rows))
-    cur.close
-    connct.close
+    cur.close()
+    connct.close()
