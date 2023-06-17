78b36e0624caab05696544f9702f22c74539c3c5
Frank <wanyoike39@gmail.com>
2023-06-17 14:08:53 +0300

    task 13

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script to replace new object of state by id
+in hbtn_0e_6_usa using sqlalchemy"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3], pool_pre_ping=True))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    sess = Session()
+    state = sess.query(State).filter_by(id=2).first()
+    state.name = 'New Mexico'
+    sess.commit()
+    sess.close()
