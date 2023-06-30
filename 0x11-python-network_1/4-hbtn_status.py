fa38860ab35b5e89e938a7af66f5d1c87468dc26
Frank <wanyoike39@gmail.com>
2023-06-30 12:31:46 +0300

    task 4 fix - using requests module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,4 +11,3 @@ if __name__ == "__main__":
     print('Body response:')
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body))
