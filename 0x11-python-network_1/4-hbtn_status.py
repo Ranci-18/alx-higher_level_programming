b7887f4932acf78c6d85c5eabd5696639821de8c
Frank <wanyoike39@gmail.com>
2023-06-30 12:28:21 +0300

    task 4 - using requests module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status
+using requests module"""
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    response = requests.get(url)
+    body = response.text
+    print('Body response:')
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body))
