29d69a29ba2e85941c0aa6b0c3bf531343bd846c
Frank <wanyoike39@gmail.com>
2023-07-25 13:12:46 +0300

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+request.get(url, (err, response, body) => {
+  if (response.statusCode === 200) {
+    const content = JSON.parse(body);
+    console.log(content.title);
+  } else {
+    console.log(err);
+  }
+});
