27ded839f2eb1bb18dde89844c77331040a0941e
Frank <wanyoike39@gmail.com>
2023-07-25 12:48:07 +0300

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const reqst = require('request');
+const url = process.argv[2];
+reqst.get(url, (err, response) => {
+    if (err) {
+	console.log(err);
+    } else {
+	console.log(`code: ${response.statusCode}`);
+    }
+});
