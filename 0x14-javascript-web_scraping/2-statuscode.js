1175a31a4811eac7a23dcfd40454e1dfbdc24708
Frank <wanyoike39@gmail.com>
2023-07-25 12:49:04 +0300

    task 2 - semistandard fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@
 const reqst = require('request');
 const url = process.argv[2];
 reqst.get(url, (err, response) => {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log(`code: ${response.statusCode}`);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
