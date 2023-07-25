9bfccdbc7baa3809b03ba75475b7e575c9dd5dab
Frank <wanyoike39@gmail.com>
2023-07-25 14:00:57 +0300

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const characterIdUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+const url = process.argv[2];
+request.get(url, (err, response, body) => {
+  if (response.statusCode === 200) {
+    const content = JSON.parse(body);
+    let count = 0;
+    for (let i = 0; i < content.results.length; i++) {
+      if (content.results[i].characters.includes(characterIdUrl)) { count += 1; }
+    }
+    console.log(count);
+  } else {
+    console.log(err);
+  }
+});
