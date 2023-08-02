01e4edbb9f5766446b0d8d14edf1022a5d6c3a58
Frank <wanyoike39@gmail.com>
2023-08-02 15:41:49 +0300

    task 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+/* global $ */
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+    const characterName = data.name;
+    $('DIV#character').text(characterName);
+  });
+});
